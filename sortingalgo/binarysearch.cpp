e149bed32e55c0d5388aa6420d5bf3374dcbc180
madhavgaba16 <mgaba_be23@thapar.edu>
2024-08-02 10:56:06 +0530

    hello

--- sortingalgo/binarysearch.cpp
+++ sortingalgo/binarysearch.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
+class node{
+    public:
+    int data;
+    node *next;
+
+}
 
 int main() {
-    cout << "hello" << endl;
+    node *head = new node();
+    head->data= 10;
+    head->next=NULL;
+    
     return 0;
 }
