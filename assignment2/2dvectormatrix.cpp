1bcafaadc6668e93128736dc690f1f1d821aaa51
madhavgaba16 <mgaba_be23@thapar.edu>
2024-08-20 20:40:57 +0530

    2d vectors

--- assignment2/2dvectormatrix.cpp
+++ assignment2/2dvectormatrix.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main(){
     vector<vector<int> >matrix(3,vector<int>(4,1));
     for(int i =0 ;i<3;i++)
-    for(int j =0 ;j<4;j++)
+    for(int j =0 ;j<4;j++){
     cout<<matrix[i][j]<<" ";
+
+    }
+    cout<<"\n";
     return 0;
 
 }
