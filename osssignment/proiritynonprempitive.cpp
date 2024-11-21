a62bc2b049197fc479677baa10fee4348b6a32ee
madhavgaba16 <mgaba_be23@thapar.edu>
2024-11-21 17:31:00 +0530

    hello

---
+++ osssignment/proiritynonprempitive.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include<climits>
+
+using namespace std;
+class process{
+     public:
+    int id ; 
+    int bursttime;
+    int arrivaltime;
+    int priority;
+    int remainingtime;
+    int compeletetime;
+    int througharoundtime;
+    int waitingtime;
+   
+   process(int in,int arrival,int burst,int priori):id(in),arrivaltime(arrival),bursttime(burst),priority(priori),remainingtime(burst),waitingtime(0),compeletetime(0),througharoundtime(0){}
+};
+void averagewaitingtime(vector<process>processes){
+    float answer=0 ;
+    for(int i =0 ;i<processes.size();i++){
+        answer+=processes[i].waitingtime;
+    }
+    answer = answer/processes.size();
+    cout<<"average waiting time :"<<answer<<endl;
+}
+void printsequence(vector<int>sequence){
+    for(int i =0 ;i<sequence.size();i++){
+        cout<<"P"<<sequence[i]+1<<" "; 
+    }
+}
+
+void priority(vector<process>processes){
+    int time =0 ;
+    int complete =0 ;
+    vector<int>sequence;
+            while(complete<processes.size()){
+                int in= -1;
+                int highpriority = INT_MIN;
+                for(int i =0;i<processes.size();i++){
+                    if(processes[i].arrivaltime<=time&&processes[i].compeletetime==0&&processes[i].priority>highpriority){
+                        in = i ;
+                        highpriority= processes[i].priority;
+                    }
+                }
+                if(in!=-1){
+                    time+=processes[in].remainingtime;
+                    processes[in].compeletetime=time;
+                    processes[in].througharoundtime= processes[in].compeletetime-processes[in].arrivaltime;
+                    processes[in].waitingtime = processes[in].througharoundtime-processes[in].bursttime;
+                    complete++;
+                     sequence.push_back(in);
+                }else{
+                    time++;
+                }
+
+            }
+            printsequence(sequence);
+            averagewaitingtime(processes);       
+}
+int main()
+{
+    int n ;
+    int resource;
+    cout<<"number of the process:"<<endl;
+    cin>>n;
+    
+    vector<process>processes;
+   for (int i = 0; i < n; ++i) {
+        int arrivalTime, burstTime,priority;
+        cout << "Enter arrival time and burst time , priority for Process P" << i + 1 << ": ";
+        cin >> arrivalTime >> burstTime>>priority;
+        process p(i + 1, arrivalTime, burstTime,priority);
+        processes.push_back(p) ;
+    }
+
+    
+    sort(processes.begin(), processes.end(), [](const process& a, const process& b) {
+        return a.arrivaltime < b.arrivaltime;
+    });
+    priority(processes);
+
+ return 0;
+}
--- osssignment/roundrobin.cpp
+++ osssignment/roundrobin.cpp
@@ -1,88 +1,88 @@
-// #include<iostream>
-// #include<vector>
-// #include<queue>
-// #include<algorithm>
-// using namespace std;
-// class process{
-//     public :
-//     int id ;
-//     int arrivaltime;
-//     int bursttime;
-//     int remainingtime;
-//     int completetime;
-//     int waitingtime;
-//     int througharoundtime;
-//     process(int inx,int arrival,int burst):id(inx), arrivaltime(arrival), bursttime(burst),
-//           remainingtime(burst), througharoundtime(0), waitingtime(0), completetime(0) {}
-// };
-// void averagewaitingtime(vector<process>processes){
-//     float answer =0 ;
-//     for(int i =0 ;i<processes.size();i++){
-//         answer+=processes[i].waitingtime;
-//     }
-//     cout<<answer/processes.size();
-// }
-// void printsequence(vector<int>sequence){
-//     int i =0;
-//     for(i;i<sequence.size();i++){
-//         cout<<sequence[i]<<" ";
-//     }
+    // #include<iostream>
+    // #include<vector>
+    // #include<queue>
+    // #include<algorithm>
+    // using namespace std;
+    // class process{
+    //     public :
+    //     int id ;
+    //     int arrivaltime;
+    //     int bursttime;
+    //     int remainingtime;
+    //     int completetime;
+    //     int waitingtime;
+    //     int througharoundtime;
+    //     process(int inx,int arrival,int burst):id(inx), arrivaltime(arrival), bursttime(burst),
+    //           remainingtime(burst), througharoundtime(0), waitingtime(0), completetime(0) {}
+    // };
+    // void averagewaitingtime(vector<process>processes){
+    //     float answer =0 ;
+    //     for(int i =0 ;i<processes.size();i++){
+    //         answer+=processes[i].waitingtime;
+    //     }
+    //     cout<<answer/processes.size();
+    // }
+    // void printsequence(vector<int>sequence){
+    //     int i =0;
+    //     for(i;i<sequence.size();i++){
+    //         cout<<sequence[i]<<" ";
+    //     }
 
-// }
-//  void roundrobin(vector<process>&processes,int quantum){
-//     int time =0 ;
-//     int complete=0 ;
-//     vector<int>sequence;
-//     queue<int>q;
-//     for(int i =0 ;i<processes.size();i++){
-//         q.push(i); 
-//     }
-//  while(complete!=processes.size()){
-//     int index = q.front();
-//     q.pop();
-//     if(processes[index].remainingtime>quantum){
-//         time+=quantum;
-//         processes[index].remainingtime-=quantum;
-//         q.push(index);
-//     }else{
-//         time+=processes[index].remainingtime;
-//         processes[index].remainingtime=0;
-//         processes[index].completetime= time;
-//         processes[index].througharoundtime= processes[index].completetime-processes[index].arrivaltime;
-//         processes[index].waitingtime= processes[index].througharoundtime-processes[index].bursttime;
-//         complete++;
-//     }
-//     sequence.push_back(index);
+    // }
+    //  void roundrobin(vector<process>&processes,int quantum){
+    //     int time =0 ;
+    //     int complete=0 ;
+    //     vector<int>sequence;
+    //     queue<int>q;
+    //     for(int i =0 ;i<processes.size();i++){
+    //         q.push(i); 
+    //     }
+    //  while(complete!=processes.size()){
+    //     int index = q.front();
+    //     q.pop();
+    //     if(processes[index].remainingtime>quantum){
+    //         time+=quantum;
+    //         processes[index].remainingtime-=quantum;
+    //         q.push(index);
+    //     }else{
+    //         time+=processes[index].remainingtime;
+    //         processes[index].remainingtime=0;
+    //         processes[index].completetime= time;
+    //         processes[index].througharoundtime= processes[index].completetime-processes[index].arrivaltime;
+    //         processes[index].waitingtime= processes[index].througharoundtime-processes[index].bursttime;
+    //         complete++;
+    //     }
+    //     sequence.push_back(index);
 
-//  }
-//  printsequence(sequence);
-//  averagewaitingtime(processes);
+    //  }
+    //  printsequence(sequence);
+    //  averagewaitingtime(processes);
 
-//  }
-// int main()
-// {
-//     int n;
-//     cout << "Enter the number of processes: ";
-//     cin >> n;
-//     int quantum;
-//      cout << "Enter the time quantum: ";
-//     cin >> quantum;
+    //  }
+    // int main()
+    // {
+    //     int n;
+    //     cout << "Enter the number of processes: ";
+    //     cin >> n;
+    //     int quantum;
+    //      cout << "Enter the time quantum: ";
+    //     cin >> quantum;
 
-//     vector<process> processes;
-//     for (int i = 0; i < n; ++i) {
-//         int arrivalTime, burstTime;
-//         cout << "Enter arrival time and burst time for Process P" << i + 1 << ": ";
-//         cin >> arrivalTime >> burstTime;
-//         process p(i + 1, arrivalTime, burstTime);
-//         processes.push_back(p);                  
-//     }
-//     sort(processes.begin(), processes.end(), [](const process& a, const process& b) {
-//         return a.arrivaltime < b.arrivaltime;
-//     });
-//     roundrobin(processes,quantum);
-    
-//  return 0;
-// }
+    //     vector<process> processes;
+    //     for (int i = 0; i < n; ++i) {
+    //         int arrivalTime, burstTime;
+    //         cout << "Enter arrival time and burst time for Process P" << i + 1 << ": ";
+    //         cin >> arrivalTime >> burstTime;
+    //         process p(i + 1, arrivalTime, burstTime);
+    //         processes.push_back(p);                  
+    //     }
+    //     sort(processes.begin(), processes.end(), [](const process& a, const process& b) {
+    //         return a.arrivaltime < b.arrivaltime;
+    //     });
+    //     roundrobin(processes,quantum);
+        
+    //  return 0;
+    // }
 #include <iostream>
 #include <vector>
 #include <queue>
